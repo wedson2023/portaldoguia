d00fd80025da736348a337cdc086a751be3f228e
Wedson Santos <wedson_cross@hotmail.com>
2018-08-16 07:04:55 -0300

    Finalizado g1 noticias, pontos turisticos e cursos da página principal

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpService } from '../http.service';
 declare var $ :any;
 @Component({
   selector: 'app-footer',
@@ -9,7 +11,30 @@ export class FooterComponent implements OnInit {
 
   @Input('imagens') imagens;
 
-  constructor() {}
+  private g1;
+  constructor(
+    private http: HttpService,
+    private router: Router
+  ) {
+    this.http.ApiGet('page/get-footer').subscribe((response:any) => {
+      let noticia = [];
+
+      for(let x in response.resposta.item)
+      {
+        let data = new Date(response.resposta.item[x].pubDate);
+
+        response.resposta.item[x].pubDate = data.getHours() + 'h' + data.getMinutes();
+        response.resposta.item[x].title = response.resposta.item[x].title.substr(0, 50);
+        noticia.push(response.resposta.item[x]);
+
+        if(parseInt(x) >= 3)
+        {
+          this.g1 = noticia;
+          return false;
+        }
+      }
+    });
+  }
 
   ngOnInit() {
     $('a.agenda').on('click', function(){
