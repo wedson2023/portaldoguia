288e47eee8c558cf5a63780f3f1a86d33bf75ff6
Wedson Santos <wedson_cross@hotmail.com>
2018-09-05 07:02:10 -0300

    Página cadastro contato funcionando

--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, AfterContentInit } from '@angular/core';
 import { NgProgressService } from 'ng2-progressbar';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-contato',
@@ -7,13 +8,46 @@ import { NgProgressService } from 'ng2-progressbar';
   styleUrls: ['./contato.component.sass']
 })
 export class ContatoComponent implements OnInit, AfterContentInit {
-
-  constructor(private progresso: NgProgressService) {
+  contato = {
+    nome : null,
+    email : null,
+    telefone : null,
+    estado : null,
+    cidade : null,
+    mensagem : null,
+  };
+  constructor(
+    private progresso: NgProgressService,
+    private http: HttpService
+  ) {
     this.progresso.start();
    }
 
   ngOnInit() {
-    
+  }
+
+  cadastrar(dados){
+
+    if(!this.contato.nome || !this.contato.email || !this.contato.mensagem || !this.contato.cidade || !this.contato.estado)
+    {
+      alert('Todos os campos são obrigatórios');
+    }
+    else
+    {
+      this.progresso.start();
+      this.http.ApiPost('contatos/cadastrar', dados).subscribe((response:any) => {
+        this.contato = {
+          nome : null,
+          email : null,
+          telefone : null,
+          estado : null,
+          cidade : null,
+          mensagem : null,
+        };
+        this.progresso.done();
+        alert('Cadastro realizado com sucesso.');
+      });
+    }
   }
 
   ngAfterContentInit(){
